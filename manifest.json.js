70e8a252b070b5160e0d7be617cd7075b782f3c6
hupo@iwaity.com <hupo@iwaity.com>
2022-02-08 15:12:11 +0800

    feat: wallet doc

---
+++ manifest.json.js
@@ -0,0 +1,20 @@
+{
+  "name": "FormLess-Extention",
+  "short_name": "FormLess",
+  "icons": [
+    {
+      "src": "android-chrome-192x192.png"/*tpa=https://wallet.formless.me/icons/android-chrome-192x192.png*/,
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "android-chrome-512x512.png"/*tpa=https://wallet.formless.me/icons/android-chrome-512x512.png*/,
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "start_url": "index.html"/*tpa=https://wallet.formless.me/index.html*/,
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#3eaf7c"
+}
